73cc18f8d09ad0da7b511bfcc1e078700120b3c2
TR-MAID <xpfjsgkqwkr@gmail.com>
2023-03-27 21:04:38 +0900

    feat: 메인페이지 완성

---
+++ src/components/DetailPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { useParams } from 'react-router-dom';
+import { ORIGINAL_IMG_ARRAY, PARTICIPANTS } from '../config/constants';
+import useScrollToTop from '../hooks/useScrollToTop';
+
+const DetailPage = () => {
+  const params = useParams();
+
+  useScrollToTop();
+
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        exit={{ opacity: 0 }}
+      >
+        <img
+          src={ORIGINAL_IMG_ARRAY[params.id]}
+          alt={`${PARTICIPANTS[params.id].NICKNAME}님의 원본 그림`}
+        />
+      </motion.div>
+    </>
+  );
+};
+
+export default DetailPage;
--- src/components/MainContents.jsx
+++ src/components/MainContents.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { AnimatePresence } from 'framer-motion';
 import MainTitle from './MainTitle';
 import MainPage from './MainPage';
+import DetailPage from './DetailPage';
 
 const MainContainer = styled.div`
   width: 1440px;
@@ -17,6 +18,7 @@ const MainContents = () => {
         <Routes>
           <Route path={'/'} element={<MainTitle />} />
           <Route path={'/main'} element={<MainPage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
         </Routes>
       </AnimatePresence>
     </MainContainer>
